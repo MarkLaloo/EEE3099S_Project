d11083478c8ad272db50ecc97b048ec8b4caaea1
Mark Laloo <llxmar003@myuct.ac.za>
2021-09-09 10:26:45 +0200

    added hardware files and updated sim files

--- Milestone 2/robotParameters.m
+++ Milestone 2/robotParameters.m
@@ -1,7 +1,7 @@
 %Robot Parameters
-wheelR = ; %Wheel radius (m)
-axleLength = ; %Distance between wheels (m)
-ticksPerRot = ; % Ticks per rotation for encoders
+wheelR = 0.031; %Wheel radius (m)
+axleLength = 0.136; %Distance between wheels (m)
+ticksPerRot = 20; % Ticks per rotation for encoders
 load wheelLUT %Look up table for platform motors
 Ts = 0.01; %Sample time
 load wheelMotorModel %Motor model for platform motors
